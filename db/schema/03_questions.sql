9b80091aa6f46d7f97067c7619020156c5b0e9e0
hoshaun <shaun.ho@mail.utoronto.ca>
2023-04-08 19:17:17 -0400

    create questions table

---
+++ db/schema/03_questions.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS questions CASCADE;
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  number INTEGER NOT NULL,
+  description VARCHAR(255) NOT NULL
+);
