762a877bfdd4e2e2d89ec7b0c8b8f02f212d5e8a
RorySchof <roryschofield@gmail.com>
2023-04-12 19:14:45 -0400

    Added functionality to Nav Bar

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,3 +1,5 @@
+// HOME PAGE BUTTON. WORKING
+
 $(document).ready(function() {
   const $homePageButton = $('#home-page');
   const $logoutButton = $('#logout');
@@ -5,6 +7,50 @@ $(document).ready(function() {
   $homePageButton.on('click', function() {
     document.location.href = '/';
   });
+
+  const $myquizzesButton = $('#my-quizzes');
+  // const $logoutButton = $('#logout');
+  $myquizzesButton.on('click', function() {
+    console.log("test")
+    document.location.href = '/quizzes/my-quizzes';
+  });
+
+// keep code above
+
+// MY QUIZZES BETTON
+
+// $(document).ready(function() {
+//   // const $loginButton = $('#Login');
+//   const $myquizzesButton = $('#my-quizzes');
+//   // const $logoutButton = $('#logout');
+//   $myquizzesButton.on('click', function() {
+//     console.log("test")
+//     document.location.href = '/quizzes/my-quizzes';
+//   });
+// });
+
+// MY ATTEMPTS BUTTON
+
+$(document).ready(function() {
+  const $myAttemptsButton = $('#my-attempts');
+  const $logoutButton = $('#logout');
+  $myAttemptsButton.on('click', function() {
+    document.location.href = '/quizzes/my-attempts';
+  });
+});
+
+// CREATE A QUIZZ BUTTON
+
+$(document).ready(function() {
+  const $createQuizButton = $('#create-quiz');
+  const $logoutButton = $('#logout');
+  $createQuizButton.on('click', function() {
+    document.location.href = '/quizzes/create';
+  });
+});
+
+
+  //POST BElOW - KEEP - LOGOUT BUTTON WORKING
 
   $logoutButton.on('click', function() {
     $.ajax('/logout', { method: 'POST' })
